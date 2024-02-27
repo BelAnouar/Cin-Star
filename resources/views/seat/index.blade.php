10554ecb5bff0ca1aaed65de998bc40f919df694
BelAnouar <anwarbelhassan@gmail.com>
2024-02-27 22:53:52 +0100

    a

--- resources/views/seat/index.blade.php
+++ resources/views/seat/index.blade.php
@@ -109,6 +109,16 @@
 
     </header>
 
+    <div class="min-h-screen hero" style="background-image: url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg);">
+        <div class="hero-overlay bg-opacity-60"></div>
+        <div class="text-center hero-content text-neutral-content">
+          <div class="max-w-md">
+            <h1 class="mb-5 text-5xl font-bold">Hello there</h1>
+            <p class="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+            <button class="btn btn-primary">Get Started</button>
+          </div>
+        </div>
+      </div>
     <section class="w-full m-12">
         <form action="" method="post" class="reserve-form">
             @csrf
